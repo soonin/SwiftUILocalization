2929e043c28ac8fe0c7afea22a42d13184082cfd
pooya <info@centurytrail.com>
2021-11-06 20:06:51 -0700

    Initial Commit

---
+++ SwiftUILocalization/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUILocalization
+//
+//  Created by Pooya on 2021-11-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUILocalization/SwiftUILocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILocalizationApp.swift
+//  SwiftUILocalization
+//
+//  Created by Pooya on 2021-11-06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
